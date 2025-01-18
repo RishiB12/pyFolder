e577e4375293322fb0105de0e55b0cf67b6a88a2
Rishi <bhagatrishi04@gmail.com>
2025-01-18 22:25:07 +0000

    Added a list that checks user input

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Create a list containing numbers 0 to 4
+numbers = [0, 1, 2, 3, 4]
+
+# Get user input
+user_input = int(input("Enter a number between 0 and 4: "))
+
+# Check if the number matches 2
+if user_input == 2:
+    print("Correct")
+else:
+    print("False")
